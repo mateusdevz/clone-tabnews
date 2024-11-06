161787141d35092ffcb055862b3475e50f4e4526
mateus.dev <32174387+mateusdevz@users.noreply.github.com>
2024-11-06 02:05:26 +0000

    Testa build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi, nenem! 💖 INHA, INHA, INHA... SE VOCE ME AMA, DÁ UMA RISADINHA</h1>
+    return <h1>Oi, nenem! 💖 INHA, INHA, INHA... SE VOCE ME AMA, DÁ UMA RISADINHA 💖</h1>
 }
 
 export default Home;

a6d48b5ee3bcbc4b61f305594618af5b01e855a8
mateuzdev <mhenrique977@gmail.com>
2025-03-29 13:40:12 -0300

    add `jsconfig.json`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test("get to /status", async () => {
-  const status = await fetch("http://localhost:3000/api/status");
+  const status = await fetch("http://localhost:3000/api/v1/status");
   expect(status.status).toBe(200);
 });

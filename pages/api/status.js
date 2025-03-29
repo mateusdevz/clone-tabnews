74afecd525613a44cf89702e36972ff5aec11a83
mateuzdev <mhenrique977@gmail.com>
2025-03-29 13:33:12 -0300

    add `database.js` e `.env`

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ message: "a API está ok" });
+import database from '../../infra/database.js'
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log('===== ', result.rows);
+  response.status(200).json({ message: result.rows });
 }
 
 export default status;

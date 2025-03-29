07255b1f2b757eddd7a762e595be0c38f433cb71
mateuzdev <mhenrique977@gmail.com>
2025-03-29 14:51:19 -0300

    finaliza retorno do /status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("get to /status", async () => {
-  const status = await fetch("http://localhost:3000/api/v1/status");
-  expect(status.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  expect(responseBody.dependencies.database.version).toBe('16.0')
+  expect(responseBody.dependencies.database.max_connections).toBe(100)
+  expect(responseBody.dependencies.database.opened_connections).toBe(1)
 });
+ 

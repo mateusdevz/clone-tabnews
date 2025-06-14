7810f156408f407b2054733f4ab6e67bd392aaf0
mateuzdev <mhenrique977@gmail.com>
2025-06-14 11:49:17 -0300

    estabiliza npm run test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,14 @@
 import database from "infra/database";
+import orchestrator from "tests/orchestrator.js";
 
 async function cleanDatabase() {
   await database.query('drop schema public cascade; create schema public;');
 }
 
-beforeAll(cleanDatabase);
+beforeAll(async() => {
+  await orchestrator.waitForAllServices();
+  cleanDatabase();
+});
 
 test("get to /migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");

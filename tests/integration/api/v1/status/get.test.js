60739ea719c9d245cdfc38703e54b5972e757d60
mateuzdev <mhenrique977@gmail.com>
2025-06-23 21:36:45 -0300

    test: add gherkin pattern to tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,13 +4,17 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("get to /status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous Users", () => {
+    test("Retriving status information", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(responseBody.dependencies.database.version).toBe("16.0");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
--- tests/unit/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const calculadora = require("../../models/calculadora.js");
-
-test("calculadora", () => {
-  expect(calculadora.somar(1, 2)).toBe(3);
-});

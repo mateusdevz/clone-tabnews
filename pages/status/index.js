f010b1f4da4ba9ee5e729348a76bc2e27b51b196
mateuzdev <mhenrique977@gmail.com>
2025-06-27 20:59:06 -0300

    fix: lint

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,69 +1,68 @@
-import useSwr from 'swr';
+import useSwr from "swr";
 
 async function fetchApi(key) {
-    const response = await fetch(key);
-    const responseBody = await response.json();
-    console.log(responseBody);
-    
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  console.log(responseBody);
 
-    return responseBody;
+  return responseBody;
 }
 
 export default function StatusPage() {
-    return (
-        <>
-            <h1>Status</h1>
-            <DatabaseInfo />
-            <UpdatedAt />
-        </>
-    )
-};
+  return (
+    <>
+      <h1>Status</h1>
+      <DatabaseInfo />
+      <UpdatedAt />
+    </>
+  );
+}
 
 function UpdatedAt() {
-    const { isLoading, data } = useSwr("/api/v1/status", fetchApi, {
-        refreshInterval: 2000,
-    });
+  const { isLoading, data } = useSwr("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
 
-    let UpdatedAtText = 'Carregando...';
+  let UpdatedAtText = "Carregando...";
 
-    if(!isLoading && data) {
-        UpdatedAtText = new Date(data.updated_at).toLocaleString('pt-BR');
-    }
+  if (!isLoading && data) {
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
 
-    return <div>Última atualização: {UpdatedAtText}</div>
+  return <div>Última atualização: {UpdatedAtText}</div>;
 }
 
 function DatabaseInfo() {
-    const { isLoading, data } = useSwr("/api/v1/status", fetchApi, {
-        refreshInterval: 2000,
-    });
+  const { isLoading, data } = useSwr("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
 
-    let maxConnections = 'Carregando...';
-    let openedConnections = 'Carregando...';
-    let dbVersion = 'Carregando...';
+  let maxConnections = "Carregando...";
+  let openedConnections = "Carregando...";
+  let dbVersion = "Carregando...";
 
-    if(!isLoading && data) {
-        maxConnections = data.dependencies.database.max_connections;
-        openedConnections = data.dependencies.database.opened_connections;
-        dbVersion = data.dependencies.database.version;
-    }
+  if (!isLoading && data) {
+    maxConnections = data.dependencies.database.max_connections;
+    openedConnections = data.dependencies.database.opened_connections;
+    dbVersion = data.dependencies.database.version;
+  }
 
-    return (
+  return (
+    <ul>
+      <li>
+        Banco de dados
         <ul>
-            <li>
-                Banco de dados
-                <ul>
-                    <li>
-                        <span>Conexões máximas: {maxConnections}</span>
-                    </li>
-                    <li>
-                        <span>Conexões abertas: {openedConnections}</span>
-                    </li>
-                    <li>
-                        <span>Versão do banco de dados: {dbVersion}</span>
-                    </li>
-                </ul>
-            </li>
+          <li>
+            <span>Conexões máximas: {maxConnections}</span>
+          </li>
+          <li>
+            <span>Conexões abertas: {openedConnections}</span>
+          </li>
+          <li>
+            <span>Versão do banco de dados: {dbVersion}</span>
+          </li>
         </ul>
-    );
+      </li>
+    </ul>
+  );
 }

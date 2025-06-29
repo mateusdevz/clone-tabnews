ea20548df48b117095d256627666a470fce3fd67
mateuzdev <mhenrique977@gmail.com>
2025-06-29 02:11:21 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

---
+++ infra/errors.js
@@ -0,0 +1,20 @@
+export class InternalServerError extends Error {
+    constructor({ cause }) {
+        super("Um erro interno não esperado aconteceu.", {
+            cause,
+        });
+
+        this.name = "InternalServerError";
+        this.action = "Entre em contato com o suporte.";
+    }
+
+    toJSON() {
+        return {
+            name: this.name,
+            message: this.message,
+            action: this.action,
+            status_code: 500,
+            cause: this.cause
+        };
+    }
+}

128e3c55d4d0f4537f10e25be0f8085fa01fbf56
mateuzdev <mhenrique977@gmail.com>
2025-07-01 20:56:05 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -81,3 +81,25 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Informação não encontrada no sistema.", {
+      cause,
+    });
+
+    this.name = "NotFoundError";
+    this.action = action;
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+      cause: this.cause,
+    };
+  }
+}
